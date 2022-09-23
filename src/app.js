714071cb28657a5313fc8dd077691dd31616c0c8
cong <cong@email.com>
2022-09-23 10:39:40 +0700

    Implement an error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
-
 const contactController = require('./controllers/contact.controller');
+const ApiError = require('./api-error');
+// const { application } = require('express');
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message: 'Welcome to contact book application.' });
-}
-);
-
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to contact book application.' });
-    });
+    res.json ({ message: 'Welcome to contact book application.'});
+});
 
 app.route('/api/contacts')
     .get(contactController.findAll)
@@ -24,10 +19,23 @@ app.route('/api/contacts')
 
 app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
 
+app.route('/api/contatcs/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
 app.route('/api/contacts/:id')
     .get(contactController.findOne)
     .put(contactController.update)
     .delete(contactController.delete);
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'));
+});
+    
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || 'Internal Server Error',
+    });
+});
 module.exports = app;
-
